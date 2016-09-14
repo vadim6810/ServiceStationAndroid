1e0c0a9710dae69361e351a094c496b50772b479
Maxim glukhov <maximglukhov@hotmail.com>
2016-09-15 00:02:42 +0300

    Add sign in/up layout at the bottom of the screen
    Adds a FrameLayout at the bottom of activity_client_main with Sign In
    and Sign Up buttons.
    
    Both buttons have transparent backgrounds. v21 drawable version features
    a ripple.
    Colors are subject to change.

--- app/src/main/java/il/co/tel_ran/carservice/ClientMainActivity.java
+++ app/src/main/java/il/co/tel_ran/carservice/ClientMainActivity.java
@@ -3,6 +3,7 @@ package il.co.tel_ran.carservice;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class ClientMainActivity extends AppCompatActivity {
 
@@ -12,6 +13,12 @@ public class ClientMainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_client_main);
 
         setupActionBar();
+    }
+
+    public void showSignUpForm(View view) {
+    }
+
+    public void showSignInForm(View view) {
     }
 
     private void setupActionBar() {
