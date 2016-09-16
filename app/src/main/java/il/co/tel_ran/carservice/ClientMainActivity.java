54e21c083dc9949add5c8e36d8349aee7a5d4f44
Maxim glukhov <maximglukhov@hotmail.com>
2016-09-16 23:29:42 +0300

    Add tabs to client_activity_main
    Add Recent Services, Find Services and Request Services.
    Each tab is implemented as fragment with their individual layout.

--- app/src/main/java/il/co/tel_ran/carservice/ClientMainActivity.java
+++ app/src/main/java/il/co/tel_ran/carservice/ClientMainActivity.java
@@ -1,6 +1,11 @@
 package il.co.tel_ran.carservice;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -13,6 +18,7 @@ public class ClientMainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_client_main);
 
         setupActionBar();
+        setupTabLayout();
     }
 
     public void showSignUpForm(View view) {
@@ -24,5 +30,54 @@ public class ClientMainActivity extends AppCompatActivity {
     private void setupActionBar() {
         Toolbar actionBar = (Toolbar) findViewById(R.id.actionBar);
         setSupportActionBar(actionBar);
+    }
+
+    private void setupTabLayout() {
+        ViewPager tabsViewPager = (ViewPager) findViewById(R.id.tabs_viewpager);
+        tabsViewPager.setAdapter(new TabsPagerAdapter(getSupportFragmentManager()));
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(tabsViewPager);
+    }
+
+    private class TabsPagerAdapter extends FragmentStatePagerAdapter {
+
+        private static final int TAB_COUNT = 3;
+
+        private String[] tabTitles = new String[TAB_COUNT];
+
+        public TabsPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+
+            tabTitles[0] = getString(R.string.tab_title_recent_services);
+            tabTitles[1] = getString(R.string.tab_title_find_services);
+            tabTitles[2] = getString(R.string.tab_title_request_service);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    // Recent services
+                    return new RecentServicesTabFragment();
+                case 1:
+                    // Search for service
+                    return new SearchServiceTabFragment();
+                case 2:
+                    // Request service
+                    return new RequestServiceTabFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return TAB_COUNT;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabTitles[position];
+        }
     }
 }
---
+++ app/src/main/java/il/co/tel_ran/carservice/RecentServicesTabFragment.java
@@ -0,0 +1,20 @@
+package il.co.tel_ran.carservice;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Max on 16/09/2016.
+ */
+public class RecentServicesTabFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tab_recent_services, container, false);
+    }
+}
