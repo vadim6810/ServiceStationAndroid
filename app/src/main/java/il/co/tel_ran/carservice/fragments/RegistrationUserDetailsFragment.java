3c5103b1e2f17d69504f7100e854d6188ba4366b
Max Glukhov <maximglukhov@hotmail.com>
2016-10-24 19:42:21 +0300

    Add base user details fragment

---
+++ app/src/main/java/il/co/tel_ran/carservice/fragments/RegistrationUserDetailsFragment.java
@@ -0,0 +1,13 @@
+package il.co.tel_ran.carservice.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by maxim on 10/24/2016.
+ */
+
+/**
+ * A base class for other types of user-type-specific fragment to inherit from.
+ */
+public class RegistrationUserDetailsFragment extends Fragment {
+}
